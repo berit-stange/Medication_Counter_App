34be3500c7d9e02632fd61cb36c351851d837541
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-09-30 20:04:16 +0200

    created script.js with eventListener

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector(".btn-morning");
+// const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
+
+changeColor.addEventListener('click', function () {
+    // if...
+    button.classList.add("change");
+    // text.classList.add("change");
+    // else...
+    // text.classList.remove("change");
+});
