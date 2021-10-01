66372ed26be5d955bdecd56fda48449dc63b910d
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-10-01 14:57:44 +0200

    added event listeners to 3 buttons + changed background-color

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-const button = document.querySelector(".btn-morning");
-// const text = document.querySelector(".title");
+const btn = document.querySelector('.btn-morning');
 const changeColor = document.querySelector(".changeColor");
-
 changeColor.addEventListener('click', function () {
-    // if...
-    button.classList.add("change");
-    // text.classList.add("change");
-    // else...
-    // text.classList.remove("change");
+    btn.classList.toggle("change");
+});
+
+const btn2 = document.querySelector('.btn-noon');
+const changeColor2 = document.querySelector(".changeColor2");
+changeColor2.addEventListener('click', function () {
+    btn2.classList.toggle("change");
+});
+
+const btn3 = document.querySelector('.btn-night');
+const changeColor3 = document.querySelector(".changeColor3");
+changeColor3.addEventListener('click', function () {
+    btn3.classList.toggle("change");
 });
